389b5689ebf812fc1ea8e847b2bce30d0102ede7
Abrahan <abrahanpiloto@gmail.com>
2025-07-27 12:13:50 -0500

    arreglos en paypal titulo

--- src/components/PayPalButton.jsx
+++ src/components/PayPalButton.jsx
@@ -3,13 +3,13 @@ import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 export default function PayPalButton() {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-[#DDE2F5]">
-      <h1 className="text-2xl font-semibold">
-        Por favor elige tu metodo de pago
-      </h1>
       <PayPalScriptProvider
         options={{ clientId: import.meta.env.VITE_PAYPAL_CLIENT }}
       >
-        <div className="w-11/12 sm:w-96 lg:w-[500px] mt-10">
+        <div className="w-11/12 sm:w-96 lg:w-[500px]">
+          <h1 className="text-2xl font-semibold mb-10">
+            Por favor elige tu metodo de pago
+          </h1>
           <PayPalButtons style={{ layout: "vertical" }} />
         </div>
       </PayPalScriptProvider>
